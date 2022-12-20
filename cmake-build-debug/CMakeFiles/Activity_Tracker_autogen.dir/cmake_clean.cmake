a650ce0fff53a093c77ad09b8154df86a9f09b4a
Francesco Vici <107649860+FrancescoVici@users.noreply.github.com>
2022-12-20 17:29:12 +0100

    Delete cmake-build-debug directory

--- cmake-build-debug/CMakeFiles/Activity_Tracker_autogen.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "Activity_Tracker_autogen/mocs_compilation.cpp"
-  "CMakeFiles/Activity_Tracker_autogen"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/Activity_Tracker_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
